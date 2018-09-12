6b9bc85b4d129e90a4c88725b69ef799cf9070bf
AndyRosenberg <andynaterosenberg@gmail.com>
2018-09-12 16:46:14 -0700

    minor cleanup

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,37 +4,8 @@ class Player < ActiveRecord::Base
   default_scope { order(id: :asc) }
   delegate :league_type, to: :lineup
 
-  def self.fake(hsh, pname = 'display_name', ppos = 'position', pid = 'player_id')
-    Player.new(full_name: hsh[pname], position: hsh[ppos], ff_id: hsh[pid])
-  end
-
-  def self.fake_show(hsh)
-    fake(hsh, 'full_name', 'position', 'ff_id')
-  end
-
-  def schedule(pteam = nil)
-    pteam ||= self.team
-    my_sched = Statistic.schedules.select do |game|
-      game['game_week'] == CURRENT_WEEK.to_s &&
-      (game['home_team'] == pteam || game['away_team'] == pteam)
-    end
-
-    if my_sched.empty? 
-      'BYE' 
-    else
-      game = my_sched.first
-      game['home_team'] == pteam ? game['away_team'] : "@#{game['home_team']}"
-    end
-  end
-
   def last_5
     Statistic.last_5.map do |k, v|
-      {k => v.find {|pl| pl['ff_id'] == ff_id} }
-    end
-  end
-
-  def this_year_total
-    Statistic.this_year.map do |k, v|
       {k => v.find {|pl| pl['ff_id'] == ff_id} }
     end
   end
@@ -75,7 +46,7 @@ class Player < ActiveRecord::Base
   end
 
   def weekly(type = nil)
-    type = (type || league_type)
+    type ||= league_type
     pj = Statistic.weekly[position].find {|plyr| plyr['player_id'] == ff_id }
     pj ? pj[type] : "0"
   end
@@ -101,6 +72,29 @@ class Player < ActiveRecord::Base
   def injuries
     inj = Statistic.injuries.find { |pl| pl['player_id'] == ff_id }
     inj ? "#{inj['game_status'].split(' ').first} - #{inj['injury']}" : "None"
+  end
+
+  def schedule(pteam = nil)
+    pteam ||= self.team
+    my_sched = Statistic.schedules.select do |game|
+      game['game_week'] == CURRENT_WEEK.to_s &&
+      (game['home_team'] == pteam || game['away_team'] == pteam)
+    end
+
+    if my_sched.empty? 
+      'BYE' 
+    else
+      game = my_sched.first
+      game['home_team'] == pteam ? game['away_team'] : "@#{game['home_team']}"
+    end
+  end
+
+  def self.fake(hsh, pname = 'display_name', ppos = 'position', pid = 'player_id')
+    Player.new(full_name: hsh[pname], position: hsh[ppos], ff_id: hsh[pid])
+  end
+
+  def self.fake_show(hsh)
+    fake(hsh, 'full_name', 'position', 'ff_id')
   end
 
   private

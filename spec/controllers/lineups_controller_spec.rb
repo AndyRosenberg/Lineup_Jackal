05bf6f8145b40bc6d898fe2b5352e52c252f7acc
AndyRosenberg <andynaterosenberg@gmail.com>
2018-10-29 13:05:08 -0700

    Demo controller tests for lineups

--- spec/controllers/lineups_controller_spec.rb
+++ spec/controllers/lineups_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe LineupsController do
   add_user
+  add_lineup
 
   context "pre-login" do
     describe "GET index" do
@@ -15,12 +16,35 @@ describe LineupsController do
   context "after-login" do
     before do
       session[:user_id] = andy.id
+      assign_lineup(andy, my_lineup)
     end
 
     describe "GET index" do
       it "loads index" do
         get :index
         expect(response).to render_template :index
+      end
+
+      it "assigns lineups" do
+        get :index
+        expect(assigns(:lineups).first).to be_instance_of(Lineup)
+      end
+    end
+
+    describe "GET show" do
+      it "has no starters" do
+        get :show, params: { id: my_lineup.token }
+        expect(assigns(:starters).size).to eq(0)
+      end
+
+      it "now has one starter" do
+        luck = Player.fake(Statistic.find_player('1932'))
+        im_luck = Player.fake_show(JSON.parse(luck.to_json))
+        im_luck.lineup_id = my_lineup.id
+        im_luck.status = "starter"
+        im_luck.save!
+        get :show, params: { id: my_lineup.token }
+        expect(assigns(:starters).size).to eq(1)
       end
     end
   end
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -8,6 +8,11 @@ def add_lineup
   let(:my_lineup) { Fabricate(:lineup) }
 end
 
+def assign_lineup(user, lineup)
+  lineup.user_id = user.id
+  lineup.save!
+end
+
 def add_5_players(lineup)
   5.times do
     pl = Fabricate(:player)
